7b36b91dcc0f44378fa1bc1d65d3316978f5eecb
Megsie Siple <mcsiple@gmail.com>
2017-08-08 14:18:44 -0700

    added wmr to calcs, sample plots

--- Corrs/WaveletExperiment.R
+++ Corrs/WaveletExperiment.R
@@ -1,13 +1,6 @@
 # install.packages("mvcwt")
 library(mvcwt)
 
-# Function takes sequences as columns and compute continuous wavelet transform of each 
-    # mvcwt(x, y, scale.exp = 0.5, nscales = get.nscales(x),
-    #       min.scale = get.min.scale(x), max.scale = get.max.scale(x),
-    #       scales = log2Bins(min.scale, max.scale, nscales), loc = regularize(x),
-    #       wave.fun = "Morlet")
-
-
 # Example from vignette ---------------------------------------------------
 data(lrlake)
 x = subset(lrlake, Basin == "Treatment", LRL.Day) / 365.25 # Time series sampling points (day of year in this case)
@@ -27,33 +20,84 @@ lines(1:nrow(y),y[,2],col='red')
 # Five LMEs: NE Atlantic, Benguela, California, Kuroshio-Oyashio, Humboldt
 load("/Users/mcsiple/Dropbox/Chapter3-SardineAnchovy/R files/allsardineanchovy.RData")
 source("/Users/mcsiple/Dropbox/Chapter3-SardineAnchovy/Code_SA/sardine-anchovy/Corrs/getMARSSstates.R")
-unique(alldat$region)
+data.list <- list()
+region.list <- list()
+regions <- unique(alldat$region)
+dsources <- unique(alldat$datasource)
+d=2 # Start with 
+#for (d in 1:length(dsources)){
+ for(r in 1:length(regions)){
 # Need to fix FAO data before using this function on them.
       var.list <- list()
-      region = "Benguela" # For Humboldt: warning for ssb and landings, not recruitment
-      data_source = "RAM"
+      region = regions[r]
+      data_source = dsources[d]
       variables = c("rec","ssb","landings")
       for(v in 1:3){
       var.list[[v]] <- getMARSSstates(data = alldat,
                                  region_or_subregion = region,
                                  scale = "Region", 
-                                 data_source = "RAM",
+                                 data_source = data_source,
                                  variable = variables[v],
                                  ccf.calc = FALSE)
       }
-      names(var.list) = variables
-      data.points <- subset(alldat, region == "Benguela", datasource = data_source)
+      region.list[[r]] <- do.call(rbind,var.list)
+ }
+
+
+
+Barange <- do.call(rbind,region.list)
+RAM <- do.call(rbind,region.list[1:3])
+save(Barange,file = "BarangeStates.RData")
+save(RAM, file = "RAMStates.RData")
+
+
+
+# Start here if you don’t need to re-process anything! --------------------
+load("BarangeStates.RData")
+load("RAMStates.RData")
+
+
+      data.points <- subset(Barange, region == "Benguela" & variable == "landings")
       # RAM only has both sardine and anchovy for any of the variables (e.g.,ss, rec) for Humboldt. 
       
-      x = var.list$rec$Year # Time series sampling points (day of year in this case)
-      y = var.list$rec[,c("Sardine.est","Anchovy.est")] # Time series only (each column = one species)
+      x = data.points$Year       # Time series sampling points (day of year in this case)
+      y = data.points[,c("Sardine.est","Anchovy.est")] # Time series only (each column = one species)
     
-      w = mvcwt(x, y, min.scale = 0.25, max.scale = 4)
+      w = mvcwt(x, y, min.scale = 1, max.scale = 20)
       mr = wmr(w)
+      #mr.boot = wmr.boot(w, smoothing = 1, reps = 1000, mr.func = "wmr")
       image(mr, reset.par = FALSE)
       contour(mr, bound = NA, add = TRUE)
       
+
+      par(mfrow=c(4,1))
       
-      plot(1:nrow(y),y[,1],type='l',ylim=c(0,max(c(y[,1],y[,2]))))
+      plot(1:nrow(y),y[,1],type='l',ylim=c(-2,max(c(y[,1],y[,2]))))
       lines(1:nrow(y),y[,2],col='red')
+  
+      # Scale: <5 yr    
+      mr$z[mr$z>0.95] <- NA
+      ind <- which(mr$y < 5)
+      trim.z <- mr$z[nrow(mr$z)-ind,,1]
+      hist(trim.z,xlim=c(0,1),col="lightblue",probability = T,main='')
+      text(0.8,1,"<5 yr")
+      
+      # Scale: 5-10 yr
+      ind2 <- which(mr$y > 5 & mr$y < 10)
+      trim.z2 <- mr$z[nrow(mr$z)-ind2,,1]
+      hist(trim.z2,xlim=c(0,1),col="lightblue",probability = T,main='')
+      text(0.8,1,"5-10 yr")
+      
+      #Scale: 10+ yr
+      ind3 <- which(mr$y > 10)
+      trim.z3 <- mr$z[nrow(mr$z)-ind3,,1]
+      hist(trim.z3,xlim=c(0,1),col="lightblue",probability = T,main='',xlab="Degree of synchrony")
+      text(0.8,1,"10+ yr")
+      
+      
+      plot(response.ts[,1],type='l')
+      lines(response.ts[,2],type='l',col='red')
+      subyrs <- 1:30 #1:nrow(response.ts)
+      x <- subyrs 
+      y <- response.ts[subyrs,1:2]
       
