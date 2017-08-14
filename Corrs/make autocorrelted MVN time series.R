b7a5cfa07a8124e2b4658b7062549132d57e1fe2
Margaret Siple <mcsiple@gmail.com>
2017-08-14 13:26:11 -0700

    testing-- should bootstrapping be more within time series or among new simulated s/a pairs?

--- Corrs/make autocorrelted MVN time series.R
+++ Corrs/make autocorrelted MVN time series.R
@@ -46,57 +46,63 @@ lines(1:100, y[,2], type = "l", lwd = 2, col = "gray")
 
 # Test whether CIs for variance should be from within the same time series or between different ones.
 
-within <- between <- vector(length = 100)
-true.covar <- seq(-1,1,length.out = 100)
+within <- between <- matrix(NA,nrow = 20,ncol = 100) # ncol = sims, nrow = how many correlations
+true.covar <- seq(-1,1,length.out = 20)
 
 set.seed(123)
-years.of.data <- 40 # How many years of data you have
+years.of.data <- 40     # How many years of data you have
 
-for(i in 1:100){ 
+# Among simulations
+for(c in 1:20){  #loop thru true covariances
   Sigma <- matrix(0, nrow=2, ncol=2)
   diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for anchovy and sardine in chapter 3
   
-  Sigma[1,2]<- true.covar[i] * Sigma[1,1]  # Add some negative correlation between the two time series
-  Sigma[2,1]<- true.covar[i] * Sigma[1,1]
+  Sigma[1,2]<- true.covar[c] * Sigma[1,1]  # Add some negative correlation between the two time series
+  Sigma[2,1]<- true.covar[c] * Sigma[1,1]
   
   # calculate variance of AR(1)
   # beta is the kroneker product of the rho matrix 
   beta <- rho  %x% rho
   I <- matrix(0,nrow=nrow(beta),ncol=ncol(beta))
-  diag(I)<-1
+  diag(I) <- 1
   Omega <- matrix(solve((I-beta)) %*% as.vector(Sigma) , 
                   nrow=2, 
                   ncol =2, 
                   byrow = F)
+  # Loop through sims
+  for(sim in 1:100){
   y <- matrix(NA, nrow = 100, ncol=2)
-  # get first year
-  for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
-  eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
-  
-  # loop through years
-  for (i in 2:100) {
-    eta.t1 <- y[i-1,]
-    eta.t <- diag(rho) * eta.t1 + eps[i,]
-    y[i,]<- eta.t
+    for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
+    eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
+      # loop through years
+      for (i in 2:100) {
+        eta.t1 <- y[i-1,]
+        eta.t <- diag(rho) * eta.t1 + eps[i,]
+        y[i,]<- eta.t
+      }
+    test <- y[1:years.of.data,] 
+    MAR.obj <- rbind(test[,1],test[,2])    #Landings are log transformed
+    model.sa=list()
+    model.sa$Q="unconstrained"
+    kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
+    covariance = kem.sa$par$Q[2]
+    correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
+    between[c,sim] <- covariance
   }
-  test <- y[1:years.of.data,] 
-  MAR.obj <- rbind(test[,1],test[,2])    #Landings are log transformed
-  model.sa=list()
-  model.sa$Q="unconstrained"
-  kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
-  covariance = kem.sa$par$Q[2]
-  correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
-  between[i] <- covariance # estimated covariance from one simulation
 }
 
-# Within
-for (i in 1:100){
+
+
+# Within ****
+# Essentially, this samples within the same time series 
+
+for(c in 1:20){  #loop thru true covariances
   Sigma <- matrix(0, nrow=2, ncol=2)
   diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for anchovy and sardine in chapter 3
   
-  Sigma[1,2]<- true.covar[i] * Sigma[1,1]  # Add some negative correlation between the two time series
-  Sigma[2,1]<- true.covar[i] * Sigma[1,1]
-  
+  Sigma[1,2]<- true.covar[c] * Sigma[1,1]  # Add some negative correlation between the two time series
+  Sigma[2,1]<- true.covar[c] * Sigma[1,1]
+
   # calculate variance of AR(1)
   # beta is the kroneker product of the rho matrix 
   beta <- rho  %x% rho
@@ -107,25 +113,35 @@ for (i in 1:100){
                   ncol =2, 
                   byrow = F)
   # Omega is the variance covariance of Y's
-  
-  # Set up simulation
-  par(mfrow=c(1,1))
-  nyears <- 100
-  y <- matrix(NA, nrow = 100, ncol=2)
-  # get first year
-  for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
-  eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
-  
-  # loop through years
-  for (i in 2:100) {
-    eta.t1 <- y[i-1,]
-    eta.t <- diag(rho) * eta.t1 + eps[i,]
-    y[i,]<- eta.t
-  }
-  
-  test <- y
-}
 
+  # Simulation
+    nyears <- 100
+    y <- matrix(NA, nrow = 100, ncol=2)
+    # get first year
+    for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
+    eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
+    
+    # loop through years
+    for (i in 2:100) {
+      eta.t1 <- y[i-1,]
+      eta.t <- diag(rho) * eta.t1 + eps[i,]
+      y[i,]<- eta.t
+    }
+# Sample within the time series
+    # Loop through sims
+    for(sim in 1:100){
+      start.ind <- sample(x = 1:(100-years.of.data),size = 1)
+      ind.to.sample <- start.ind:(start.ind+years.of.data)
+      (test <- y[ind.to.sample,]) # For testing, print these a bunch
+      MAR.obj <- rbind(test[,1],test[,2])    #Landings are log transformed
+      model.sa=list()
+      model.sa$Q="unconstrained"
+      kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
+      covariance = kem.sa$par$Q[2]
+      correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
+      within[c,sim] <- covariance
+    }
+}
 
 
 
