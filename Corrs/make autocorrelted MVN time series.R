03adaf493b057fc36365c07b20a8d0c8b1275971
Megsie Siple <mcsiple@gmail.com>
2017-08-13 17:46:23 -0700

    initial commit - code from TEE & MDS to simulate MVN time series

---
+++ Corrs/make autocorrelted MVN time series.R
@@ -0,0 +1,39 @@
+require(MASS)
+require(MASS)
+#matrix of autocorrelation terms 
+rho <- matrix(c(0.8,0,0,0.8),nrow=2,byrow=T)
+# Create variance covariance matrix
+Sigma <- matrix(0, nrow=2, ncol =2)
+diag(Sigma)<- c(0.8,0.7)
+
+Sigma[1,2]<--0.75 * Sigma[1,1]
+Sigma[2,1]<- -0.75 * Sigma[1,1]
+
+# calculate variance of AR(1)
+# beta is the kroneker product of the rho matrix 
+beta <- rho  %x% rho
+I <- matrix(0,nrow=nrow(beta),ncol=ncol(beta))
+diag(I)<-1
+Omega <- matrix(solve((I-beta)) %*% as.vector(Sigma) , 
+                nrow=2, 
+                ncol =2, 
+                byrow = F)
+# Omega is the variance covariance of Y's
+
+# Set up simulation
+nyears <- 100
+y <- matrix(NA, nrow = 100, ncol=2)
+# get first year
+for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
+eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
+
+# loop through years
+for (i in 2:100) {
+  eta.t1 <- y[i-1,]
+  eta.t <- diag(rho) * eta.t1 + eps[i,]
+  y[i,]<- eta.t
+}
+
+plot(1:100,y[,1], type = "l", lwd = 2, col = "black")
+lines(1:100, y[,2], type = "l", lwd = 2, col = "gray")
+
