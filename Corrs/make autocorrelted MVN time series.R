af99f6b12428f298991c4bb54e95e06d9c68ffd1
Margaret Siple <mcsiple@gmail.com>
2017-08-14 18:07:35 -0700

    comments, generate.sa fxn, corr plot

--- Corrs/make autocorrelted MVN time series.R
+++ Corrs/make autocorrelted MVN time series.R
@@ -1,10 +1,13 @@
+#######################################################################################
+# This code is modified from Tim w/ guidance from MDS. The code generates autocorrelated time series that covary with each other. You specify the covariance in the Sigma matrix, which is used to get multivariate process errors. It works nicely to create negatively covarying time series. After the initial test, there is code to figure out whether confidence intervals should be taken from resampling within the same simulated time series ("within"), or among different simulated time series ("between"). After that comparison is clear, we can pick one and use it to determine the amount of data necessary to detect a correlation, or the chance of seeing a spurious one.
+#######################################################################################
+
 require(MASS)
-require(MASS)
-#matrix of autocorrelation terms 
+# Matrix of autocorrelation terms 
 rho <- matrix(c(0.8,0,0,0.8),nrow=2,byrow=T) # used to be 0.8 and 0.8
 # Create variance covariance matrix
 Sigma <- matrix(0, nrow=2, ncol=2)
-diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for anchovy and sardine in chapter 3
+diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for both anchovy and sardine in chapter 3
 
 Sigma[1,2]<- -0.75 * Sigma[1,1]  # Add some negative correlation between the two time series
 Sigma[2,1]<- -0.75 * Sigma[1,1]
@@ -40,17 +43,113 @@ test <- y
 plot(1:100,y[,1], type = "l", lwd = 2, col = "black")
 lines(1:100, y[,2], type = "l", lwd = 2, col = "gray")
 
+# 
+#Function to do the same
+generate.sa <- function(diag.sigma = c(0.6,0.6),
+                        true.covar = 0,
+                        nyears = 100){
+  rho <- matrix(c(0.8,0,0,0.8),nrow=2,byrow=T) # used to be 0.8 and 0.8
+  # Create variance covariance matrix
+  Sigma <- matrix(0, nrow=2, ncol=2)
+  diag(Sigma) <- diag.sigma # These are the sigma values used for both anchovy and sardine in chapter 3
+  Sigma[1,2]<- true.covar * Sigma[1,1]  # Add some negative correlation between the two time series
+  Sigma[2,1]<- true.covar * Sigma[1,1]
+  # beta is the kroneker product of the rho matrix 
+  beta <- rho  %x% rho
+  I <- matrix(0,nrow=nrow(beta),ncol=ncol(beta))
+  diag(I)<-1
+  Omega <- matrix(solve((I-beta)) %*% as.vector(Sigma) , 
+                  nrow=2, 
+                  ncol =2, 
+                  byrow = F)
+  # Omega is the variance covariance of Y's
+  y <- matrix(NA, nrow = 100, ncol=2)
+  # get first year
+  for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
+  eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
+  # loop through years
+  for (i in 2:100) {
+    eta.t1 <- y[i-1,]
+    eta.t <- diag(rho) * eta.t1 + eps[i,]
+    y[i,]<- eta.t
+  }
+  return(y)
+}
+
+true.covar.vec <- c(-0.9,-0.5,0,0.5,0.9)
+tslength.vec <- 10:40
+
+for (c in 1:length(true.covar.vec)){
+  for(t in 1:length(tslength.vec)){
+  test <- generate.sa(true.covar = true.covar.vec[c])
+  test <- y[1:years.of.data,] 
+  MAR.obj <- rbind(test[,1],test[,2])  
+  model.sa=list()
+  model.sa$Q="unconstrained"
+  kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
+  covariance = kem.sa$par$Q[2]
+  correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
+  results.mat[c,t] <- correlation
+  }}
 
 
-# MARSS covariance --------------------------------------------------------
-
-# Test whether CIs for variance should be from within the same time series or between different ones.
-
+# Figure out which method to use for detecting “power" --------------------
+# This first one samples N-year chunks within one time series
+set.seed(123)
 within <- between <- matrix(NA,nrow = 20,ncol = 100) # ncol = sims, nrow = how many correlations
 true.covar <- seq(-1,1,length.out = 20)
-
-set.seed(123)
 years.of.data <- 40     # How many years of data you have
+
+for(c in 1:20){  #loop thru true covariances
+  Sigma <- matrix(0, nrow=2, ncol=2)
+  diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for anchovy and sardine in chapter 3
+  
+  Sigma[1,2]<- true.covar[c] * Sigma[1,1]  # Add some negative correlation between the two time series
+  Sigma[2,1]<- true.covar[c] * Sigma[1,1]
+  
+  # calculate variance of AR(1)
+  # beta is the kroneker product of the rho matrix 
+  beta <- rho  %x% rho
+  I <- matrix(0,nrow=nrow(beta),ncol=ncol(beta))
+  diag(I)<-1
+  Omega <- matrix(solve((I-beta)) %*% as.vector(Sigma) , 
+                  nrow=2, 
+                  ncol =2, 
+                  byrow = F)
+  # Omega is the variance covariance of Y's
+  
+  # Simulation
+  nyears <- 100
+  y <- matrix(NA, nrow = 100, ncol=2)
+  # get first year
+  for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
+  eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
+  
+  # loop through years
+  for (i in 2:100) {
+    eta.t1 <- y[i-1,]
+    eta.t <- diag(rho) * eta.t1 + eps[i,]
+    y[i,]<- eta.t
+  }
+  # Sample within the time series
+  # Loop through sims
+  for(sim in 1:100){
+    start.ind <- sample(x = 1:(100-years.of.data),size = 1)
+    ind.to.sample <- start.ind:(start.ind+years.of.data)
+    (test <- y[ind.to.sample,]) # For testing, print these a bunch
+    MAR.obj <- rbind(test[,1],test[,2])  
+    model.sa=list()
+    model.sa$Q="unconstrained" # estimate variance and covariance
+    kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
+    covariance = kem.sa$par$Q[2]
+    correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
+    within[c,sim] <- covariance
+  }
+}
+
+# It looks like within and between are similar, so we can use either method for simulating...
+# MARSS covariance --------------------------------------------------------
+# Test whether CIs for variance should be from within the same time series or between different ones.
 
 # Among simulations
 for(c in 1:20){  #loop thru true covariances
@@ -81,7 +180,7 @@ for(c in 1:20){  #loop thru true covariances
         y[i,]<- eta.t
       }
     test <- y[1:years.of.data,] 
-    MAR.obj <- rbind(test[,1],test[,2])    #Landings are log transformed
+    MAR.obj <- rbind(test[,1],test[,2])  
     model.sa=list()
     model.sa$Q="unconstrained"
     kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
@@ -92,65 +191,30 @@ for(c in 1:20){  #loop thru true covariances
 }
 
 
+# Figure  -----------------------------------------------------------------
+setwd("/Users/mcsiple/Dropbox/Chapter3-SardineAnchovy/Code_SA/sardine-anchovy/ProcData")
+#save(between,file = "Between_Var.RData")
+load("Within_Var.RData")
+meds <- apply(between,MARGIN = 1,FUN = median, na.rm=T)
+loCIs <- apply(between,MARGIN = 1, FUN = quantile, probs = c(0.05,0.95))[1,]
+hiCIs <- apply(between,MARGIN = 1, FUN = quantile, probs = c(0.05,0.95))[2,]
 
-# Within ****
-# Essentially, this samples within the same time series 
+par(mar=c(4,4,3,2))
+plot(true.covar,meds, ylim=range(c(loCIs, hiCIs)),pch=19,type='b',xlab = "True covariance",ylab="Estimated covariance")
+abline(0,1,col="grey")
+arrows(true.covar, loCIs, true.covar, hiCIs, length=0.05, angle=90, code=3)
 
-for(c in 1:20){  #loop thru true covariances
-  Sigma <- matrix(0, nrow=2, ncol=2)
-  diag(Sigma) <- c(0.6,0.6) # These are the sigma values used for anchovy and sardine in chapter 3
-  
-  Sigma[1,2]<- true.covar[c] * Sigma[1,1]  # Add some negative correlation between the two time series
-  Sigma[2,1]<- true.covar[c] * Sigma[1,1]
+meds <- apply(within,MARGIN = 1,FUN = median, na.rm=T)
+loCIs2 <- apply(within,MARGIN = 1, FUN = quantile, probs = c(0.05,0.95), na.rm=T)[1,]
+hiCIs2 <- apply(within,MARGIN = 1, FUN = quantile, probs = c(0.05,0.95), na.rm=T)[2,]
+arrows(true.covar+0.02, loCIs2, true.covar+0.02, hiCIs2, length=0.05, angle=90, code=3)
+points(true.covar+0.02,meds, ylim=range(c(loCIs2, hiCIs2)),type='b',bg='white',pch=21)
 
-  # calculate variance of AR(1)
-  # beta is the kroneker product of the rho matrix 
-  beta <- rho  %x% rho
-  I <- matrix(0,nrow=nrow(beta),ncol=ncol(beta))
-  diag(I)<-1
-  Omega <- matrix(solve((I-beta)) %*% as.vector(Sigma) , 
-                  nrow=2, 
-                  ncol =2, 
-                  byrow = F)
-  # Omega is the variance covariance of Y's
-
-  # Simulation
-    nyears <- 100
-    y <- matrix(NA, nrow = 100, ncol=2)
-    # get first year
-    for (i in 1:2) y[1,i] <- rnorm(1,0,Omega)
-    eps <- mvrnorm(n = 100,mu = c(0,0), Sigma = Sigma)
-    
-    # loop through years
-    for (i in 2:100) {
-      eta.t1 <- y[i-1,]
-      eta.t <- diag(rho) * eta.t1 + eps[i,]
-      y[i,]<- eta.t
-    }
-# Sample within the time series
-    # Loop through sims
-    for(sim in 1:100){
-      start.ind <- sample(x = 1:(100-years.of.data),size = 1)
-      ind.to.sample <- start.ind:(start.ind+years.of.data)
-      (test <- y[ind.to.sample,]) # For testing, print these a bunch
-      MAR.obj <- rbind(test[,1],test[,2])    #Landings are log transformed
-      model.sa=list()
-      model.sa$Q="unconstrained"
-      kem.sa = MARSS(MAR.obj, model=model.sa, control=list(maxit=1000),silent = TRUE) 
-      covariance = kem.sa$par$Q[2]
-      correlation = covariance / (sqrt(kem.sa$par$Q[3]) * sqrt(kem.sa$par$Q[1]))
-      within[c,sim] <- covariance
-    }
-}
+legend("topleft",legend = c("Between simulations","Within simulations"),pch=c(19,1))
 
 
-
-
-
-      #kem.sa.CIs = MARSSparamCIs(kem.sa,method="parametric",nboot=200) # if you want to get confidence intervals for covariance estimates
-      #print(kem.sa.CIs)
-
-
+#kem.sa.CIs = MARSSparamCIs(kem.sa,method="parametric",nboot=200) # if you want confidence intervals for covariance estimates
+#print(kem.sa.CIs)
 
 
 
