067c06d7fab89bce975206f15efe91ea0b37ab3c
Margaret Siple <mcsiple@gmail.com>
2017-08-22 18:26:07 -0700

    doc'ing edits

--- Corrs/Corr_Fig.R
+++ Corrs/Corr_Fig.R
@@ -6,16 +6,14 @@ library(reshape2)
 library(MARSS)
 
 corr.fig <- function(data = alldat, region_or_subregion = "Benguela", scale = "Region", data_source = "Barange", variable = "landings",MARSS.cov = FALSE, plot = FALSE, ccf.calc = FALSE){
-  # data = dataset including the time series you're interested in
-  # Region = 1 of 5 LMEs (Benguela, California, NE Atlantic, Kuroshio-Oyashio, Humboldt)
-  # variable = the variable (rec, biomass, or landings) 
-  # Datasource = FAO, RAM, or Barange
+  #'@param data = dataset including the time series you're interested in
+  #'@param Region = 1 of 5 LMEs (Benguela, California, NE Atlantic, Kuroshio-Oyashio, Humboldt)
+  #'@param variable = the variable (rec, biomass, or landings) 
+  #'@param Datasource = FAO, RAM, or Barange
   
   if (scale == "Region") {dataset <- filter(data, region == region_or_subregion & datasource==data_source)}
-  if (scale == "Subregion") {dataset <- filter(data, subregion == region_or_subregion & datasource==data_source) }   
+  if (scale == "Subregion") {dataset <- filter(data, subregion == region_or_subregion & datasource==data_source)}   
   
-  #dataset <- subset(dataset,datasource == datasource)
-  #print(dataset)
   if(variable=="landings"){
     lt.maxes <- ddply(.data=dataset,.(sp,stock),summarize,max.var=round(max(landings,na.rm=TRUE),2))} 
   if(variable=="ssb"){
@@ -59,7 +57,7 @@ corr.fig <- function(data = alldat, region_or_subregion = "Benguela", scale = "R
   
   # One correlation method: use MARSS to find covariance --------------------------------------------
   if(MARSS.cov == TRUE){
-    source("Fill_NAs_SA.R")
+    source("~/Dropbox/Chapter3-SardineAnchovy/Code_SA/sardine-anchovy/ProcData/Fill_NAs_SA.R")
     sard.mars <- FillNAs.ts(cbind(dom.s.ts$year,sar),
                             startyear=min(c(dom.s.ts$year,dom.a.ts$year)),
                             endyear=max(c(dom.s.ts$year,dom.a.ts$year)))
@@ -88,11 +86,6 @@ corr.fig <- function(data = alldat, region_or_subregion = "Benguela", scale = "R
   
   return(list(correlation = correlation,Dom_anch_LTmax=as.character(lt.max.sp),
               Dom_sard_LTmax=as.character(lt.max.sp.sar),dom.anch = dom.a.ts,dom.sard = dom.s.ts,max.table = lt.maxes))
-  
-  # print(kem.sa.CIs$par.upCI)
-  # print(kem.sa.CIs$par.lowCI)
-  #legend("topright",legend=c("sardine","anchovy"),lty=c(1,1),col=c('black','red'))
-  #table(dataset$Land.Area,dataset$Scientific.name)
   
 }  #End corr.fig function
 
