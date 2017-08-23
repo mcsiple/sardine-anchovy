d01d6b6963d7da4c88de9bc5f0abe8e01b26ae94
Megsie Siple <mcsiple@gmail.com>
2017-08-22 21:27:41 -0700

    initial commit-test different time series for frequency of asynch. dynamics

---
+++ Corrs/WMRSimulationTests.R
@@ -0,0 +1,36 @@
+# Test - quantiles for distributions of WMR under asynchronous/synchronous dynamics
+# Simulate time series as before
+
+asyn <- generate.sa(true.covar = -0.99)
+plot(1:nrow(asyn),asyn[,1],type='l')
+lines(1:nrow(asyn),asyn[,2],col="darkgrey")
+
+x <- 1:nrow(asyn)
+y <- asyn
+# wave.fun = "Morlet"
+scale.exp = 0.5; nscales = get.nscales(x)
+min.scale = get.min.scale(x); max.scale = get.max.scale(x)
+scales = log2Bins(min.scale, max.scale, nscales)
+w = mvcwt(x, y)
+mr = wmr(w)
+#image(mr, reset.par = FALSE,xlab="Year")
+#contour(mr, bound = NA, add = TRUE)
+to.trim <- round(scales) # this is the number of cells to trim from the matrix (top and bottom)
+mmat <- mr$z[,,1]
+# OMG THIS TOOK ME SO LONG
+for(c in 1:ncol(mmat)){
+  mmat[1:to.trim[c],c] <- NA
+  mmat[nrow(mmat):(nrow(mmat)-to.trim[c]),c] <- NA
+}
+# NEW: trim cone of influence from z matrix (these values don't count!)
+ind <- which(mr$y < 5)
+trim.z.list <- mmat[,ind]
+hist(trim.z.list,xlim=c(0,1))
+quantile(trim.z.list,probs=c(0.75),na.rm=T)
+#prop1[i] <- length(which(trim.z.list<0.7)) / length(which(!is.na(trim.z.list)))
+
+true.cov <- c(-0.99,-0.75,-0.5,0)
+rho <- c(0.9,0.9,0.7)
+sims = 1:100
+sim.df <- expand.grid(sims,true.cov,rho)
+sim.df[1:100,]
