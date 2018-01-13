12c3b00814b31ca5bf1c1b5149eeef238ec09490
Margaret Siple <mcsiple@gmail.com>
2018-01-12 16:49:33 -0800

    fixed file paths, fixed FAO stock column (now it is a combo of ocean area and land area)

--- ProcData/Combine all ts_v2.R
+++ ProcData/Combine all ts_v2.R
@@ -1,15 +1,18 @@
 # Combine all data from RAM, FAO, Barange et al.
-# The resulting dataset is "allsardineanchovy.RData" which is used in all subsequent analyses.
-setwd("~/Dropbox/Chapter3-SardineAnchovy/R files")
+# The resulting dataset is "allsardineanchovy.RData" which is used in all subsequent analyses
+# NOTE: One important thing this code does is turn the "NA" stocks in the FAO database into fake stocks.
+# For example, there is a time series of sardine in land area "Europe" and one in land area "-"
 
-load("~/Dropbox/Chapter3-SardineAnchovy/All Datasets/RAM/RAM.RData")      #RAM
-load("~/Dropbox/Chapter3-SardineAnchovy/All Datasets/FAO/FAO.RData")      #FAO
-load("~/Dropbox/Chapter3-SardineAnchovy/All Datasets/Barange/Barange_mystocks.RData")   #barange_noNAs
+basedir <- "~/Dropbox/Chapter3-SardineAnchovy"
+
+load(file.path(basedir,"Datasets/RAM/RAM.RData"))      #RAM
+load(file.path(basedir,"Datasets/FAO/FAO.RData"))      #FAO
+load(file.path(basedir,"Datasets/Barange/Barange_mystocks.RData"))   #barange_noNAs
 b <- which(colnames(barange_noNAs)=='b')
 colnames(barange_noNAs)[b] <- 'sp'
+
 library(plyr)
 library(dplyr)
-
 library(ggplot2)
 library(reshape2)
 
@@ -19,18 +22,41 @@ library(reshape2)
 # Stock  year  ssb   rec   landings    fishing.mortality   sp    region    subregion
 # Following format of barange_noNAs
 
-barange.new <- data.frame(datasource=rep("Barange",times=nrow(barange_noNAs)),scientificname=rep(NA,times=nrow(barange_noNAs)),barange_noNAs)
+barange.new <- data.frame(datasource=rep("Barange",times=nrow(barange_noNAs)),
+                          scientificname=rep(NA,times=nrow(barange_noNAs)),
+                          barange_noNAs)
 
-RAM.new <- data.frame(datasource=rep("RAM",times=nrow(RAM)),scientificname=RAM$scientificname,stock=RAM$stocklong,year=RAM$Years,ssb=RAM$SSB,rec=RAM$R,totalcatch=RAM$TC,fishing.mortality=RAM$F,sp=RAM$sp,region=RAM$region,subregion=rep(NA,times=nrow(RAM)))
+RAM.new <- data.frame(datasource=rep("RAM",times=nrow(RAM)),
+                      scientificname=RAM$scientificname,
+                      stock=RAM$stocklong,
+                      year=RAM$Years,
+                      ssb=RAM$SSB,
+                      rec=RAM$R,
+                      totalcatch=RAM$TC,
+                      fishing.mortality=RAM$F,
+                      sp=RAM$sp,
+                      region=RAM$region,
+                      subregion=rep(NA,times=nrow(RAM)))
 
 FAO[grep("Southern African pilchard",FAO$Species), 2] <- "Africa"
-FAO.new <- data.frame(datasource=rep("FAO",times=nrow(FAO)),scientificname=FAO$Scientific.name,stock=rep(NA,times=nrow(FAO)),year=FAO$year,ssb=rep(NA,times=nrow(FAO)),rec=rep(NA,times=nrow(FAO)),landings=FAO$landings,fishing.mortality=rep(NA,times=nrow(FAO)),sp=FAO$sp,region=FAO$region,subregion=FAO$Land.Area)
+FAO.new <- data.frame(datasource=rep("FAO",times=nrow(FAO)),
+                      scientificname=FAO$Scientific.name,
+                      stock=paste(FAO$Ocean.Area,FAO$Land.Area), # This is just so that there is a stock name - needed for future comparisons
+                      year=FAO$year,
+                      ssb=rep(NA,times=nrow(FAO)),
+                      rec=rep(NA,times=nrow(FAO)),
+                      landings=FAO$landings,
+                      fishing.mortality=rep(NA,times=nrow(FAO)),
+                      sp=FAO$sp,
+                      region=FAO$region,subregion=FAO$Land.Area)
+
 
 # Output just one species for ADMB
 # European pilchard, 34 years of data
 # Epilchard <- subset(RAM.new,scientificname=="Sardina pilchardus")
 # Ssagax <- subset(RAM.new,scientificname=="Sardinops sagax" & region =="California")
 # write.csv(x = Ssagax,file = "Sardinops_sagax.csv")
+
 colnames(barange.new)
 alldat <- rbind.fill(list(barange.new,RAM.new,FAO.new))
 alldat$year <- as.numeric(alldat$year)
