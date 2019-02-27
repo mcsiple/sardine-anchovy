aa128ef592c5e333e1b8641525f449185be52a31
mcsiple <mcsiple@gmail.com>
2019-02-27 14:52:58 -0800

    Added map material

--- Map/Maps.R
+++ Map/Maps.R
@@ -5,6 +5,21 @@ library(leaflet)
 mapwd <- "Map/LME66"
 
 lmes <- readOGR("LMEs66.shp")
+
+bins <- c(0, 200, 500, 1000, Inf)
+pal <- colorBin("YlOrRd", domain = lmes$Shape_Area, bins = bins)
+
 leaflet(lmes) %>%
-  addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+  addPolygons(
+    fillColor = ~pal(Shape_Area),
+    color = "darkgrey", 
+              weight = 1, smoothFactor = 0.5, 
               opacity = 1.0, fillOpacity = 0.5)
+
+# Look at the data
+head(lmes@data)
+
+class(lmes) # "SpatialPolygonsDataFrame"
+names(lmes)
+
+# How to color polygons by some variable? in lmes@data or otherwise
