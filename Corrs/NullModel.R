5d260a96fb56ed64fd417c2052e5ca53ef3b98c8
Lewis Barnett <labarnett@ucdavis.edu>
2019-03-21 16:15:01 -0700

    compare test results of subsampled vs full null

--- Corrs/NullModel.R
+++ Corrs/NullModel.R
@@ -125,11 +125,11 @@ test_wmr <- function(obs, null.combined){
   # get observed wmr
   m <- get_wmr(std_anchovy, std_sardine)
   
-  # use this sampled null to compare medians of observed and null wmrs
-  test.1 = wilcox.test(m$less.than.5, null.combined$less.than.5, conf.int = TRUE) 
-  test.5 = wilcox.test(m$five.ten, null.combined$five.ten, conf.int = TRUE)
-  test.10 = wilcox.test(m$ten.plus, null.combined$ten.plus, conf.int = TRUE)
-  # get relevant test information, where diff is the median of diff between samples and bounded by CI
+  # use this sampled null to compare medians of observed and null wmrs (if slow, try exact = FALSE)
+  test.1 = wilcox.test(m$less.than.5, null.combined$less.than.5, conf.int = TRUE, exact = TRUE) 
+  test.5 = wilcox.test(m$five.ten, null.combined$five.ten, conf.int = TRUE, exact = TRUE)
+  test.10 = wilcox.test(m$ten.plus, null.combined$ten.plus, conf.int = TRUE, exact = TRUE)
+  # get relevant test information, where diff is the median of diff between all pairs and bounded by CI
   test.df = data.frame(period = c("less.than.5","five.ten","ten.plus"), 
                        test.stat = c(test.1$statistic,test.5$statistic,test.10$statistic), 
                        diff = c(test.1$estimate,test.5$estimate,test.10$estimate), 
@@ -153,9 +153,9 @@ test_wmr_sub <- function(obs, null.combined, n.factor = 1){
   m.null$five.ten <- sample(null.combined$five.ten, size = n.factor * length(m$five.ten))
   m.null$ten.plus <- sample(null.combined$ten.plus, size = n.factor * length(m$ten.plus))
   # use this sampled null to compare medians of observed and null wmrs
-  test.1 = wilcox.test(m$less.than.5, m.null$less.than.5, conf.int = TRUE) 
-  test.5 = wilcox.test(m$five.ten, m.null$five.ten, conf.int = TRUE)
-  test.10 = wilcox.test(m$ten.plus, m.null$ten.plus, conf.int = TRUE)
+  test.1 = wilcox.test(m$less.than.5, m.null$less.than.5, conf.int = TRUE, exact = TRUE) 
+  test.5 = wilcox.test(m$five.ten, m.null$five.ten, conf.int = TRUE, exact = TRUE)
+  test.10 = wilcox.test(m$ten.plus, m.null$ten.plus, conf.int = TRUE, exact = TRUE)
   # get relevant test information, where diff is the median of diff between samples and bounded by CI
   test.df = data.frame(period = c("less.than.5","five.ten","ten.plus"), 
                        test.stat = c(test.1$statistic,test.5$statistic,test.10$statistic), 
@@ -168,11 +168,40 @@ test_wmr_sub <- function(obs, null.combined, n.factor = 1){
 
 # playing with tests here
 null.combined <- get_large_null(dat = RBF,dsource = "Barange",reg = "California",var = "ssb",nsims=50)
-test_wmr(obs = get_obs(dat = RBF,dsource = "Barange",reg = "California",var = "ssb"),
+full <- test_wmr(obs = get_obs(dat = RBF,dsource = "Barange",reg = "California",var = "ssb"),
          null.combined = null.combined)
-for(i in 1:20){
+test1 = test5 = test10 = diff1 = diff5 = diff10 = CL.L1 = CL.L5 = CL.L10 = vector()
+for(i in 1:100){
   blah = test_wmr_sub(obs = get_obs(dat = RBF,dsource = "Barange",reg = "California",var = "ssb"),
          null.combined = null.combined, n.factor = 1)[[1]]
-  print(blah)
+  test1 <- c(test1, blah[1,2])
+  test5 <- c(test5, blah[2,2])
+  test10 <- c(test10, blah[3,2])
+  diff1 <- c(diff1, blah[1,3])
+  diff5 <- c(diff5, blah[2,3])
+  diff10 <- c(diff10, blah[3,3])
+  CL.L1 <- c(CL.L1, blah[1,4])
+  CL.L5 <- c(CL.L5, blah[2,4])
+  CL.L10 <- c(CL.L10, blah[3,4])
 }
-# results appear consistent (diff stable), but can also try to get threshold test statistic by percentile method bootstrap
+hist(test1)
+hist(test5)
+hist(test10)
+hist(diff1)
+abline(v=full[1,3], col = "red")
+hist(diff5)
+abline(v=full[2,3], col = "red")
+hist(diff10)
+abline(v=full[3,3], col = "red")
+hist(CL.L1)
+abline(v=full[1,4], col = "red")
+hist(CL.L5)
+abline(v=full[2,4], col = "red")
+hist(CL.L10)
+abline(v=full[3,4], col = "red")
+# results appear fairly consistent among individual surrogate tests and full null test
+# diff equivalent or within 0.005 that from full null test
+# lower CL typically =< 0.01 that from full null test
+
+# could also try to get threshold values by percentile method bootstrap but seems unnecessary 
+# (test is already a form of permutation test)
