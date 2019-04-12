8ab0fafbb897086a4893e1b6a65871cba435cc0c
Megsie Siple <mcsiple@gmail.com>
2019-04-12 13:39:57 -0700

    add wrapper code for detecting asynchrony (fix later)

--- Corrs/PowerAnalysis.R
+++ Corrs/PowerAnalysis.R
@@ -1,11 +1,11 @@
 # Power Analysis!
-#library(Rmpfr)
 
-#basedir <- "C:/Users/siplem/Dropbox/Chapter3-SardineAnchovy"
-basedir <- "~/Dropbox/Chapter3-SardineAnchovy" # MAKE SURE BASEDIR MATCHES IN IN THE MAKE AUTOCORRELATED TS FILE
+basedir <- "C:/Users/siplem/Dropbox/Chapter3-SardineAnchovy"
+#basedir <- "~/Dropbox/Chapter3-SardineAnchovy" # MAKE SURE BASEDIR MATCHES IN IN THE MAKE AUTOCORRELATED TS FILE
 source(file.path(basedir,"Code_SA/sardine-anchovy/Corrs/make autocorrelated MVN time series.R"))
 #"Dropbox/Chapter3-SardineAnchovy/Code_SA/sardine-anchovy/Corrs/NullModel.R"
 source(file.path(basedir,"Code_SA/sardine-anchovy/Corrs/NullModel.R"))
+
 # outputs of get_large_null look like this:
 #  list(std_anchovy=std_anchovy,std_sardine=std_sardine)
 #outputs of get_obs() look like this:
@@ -20,8 +20,10 @@ source(file.path(basedir,"Code_SA/sardine-anchovy/Corrs/NullModel.R"))
     # }
     # return(list(Anchovy.surrogates = a.sims,Sardine.surrogates = s.sims))
 
-test.y <- generate.sa(true.covar = -.7,nyears=150)
-plot(test.y[,1],type='l')
+longyrs=150
+
+test.y <- generate.sa(true.covar = -.7,nyears=longyrs,diag.sigma=c(.8,.8))
+plot(test.y[,1],type='l',ylim=c(-4,4),ylab="Biomass")
 lines(test.y[,2],col='red')
 ts_test <- list(std_anchovy=scale(test.y[,1]),
                 std_sardine=scale(test.y[,2]))
@@ -43,5 +45,23 @@ giantnull2 <- get_large_null2(timeseries=ts_test,nsims=50)
 
 # ok, this is the null dist for the true dist. so we calculate wmr for the long time series, and this will be the d[] value that the shorter time series should converge to. 
 
-test <- test_wmr(obs = timeseries, null.combined = giantnull2)
-true.d <- list(test$diff,test$CI.L,test$CI.U)
+test <- test_wmr(obs = ts_test, null.combined = giantnull2)
+true.d <- data.frame(diff=test$diff,CI.L=test$CI.L,CI.U=test$CI.U)
+true.d$timescale <- c("less5","five10","tenplus")
+
+# Now sample from the longer time series for shorter lengths
+obslengths <- seq(10,150,by=10)
+yearindices <- 1:longyrs
+
+for(l in 1:length(obslengths)){
+  #start.ind <- sample(x = 1:(yearindices-obslengths[l])) # use this eventually
+  start.ind <- 1
+  obs <- ts_test[start.ind:obslengths[l]]
+  giantnull.obs <- get_large_null2(timeseries=obs,nsims=50)
+  wmr.obs <- test_wmr(obs = obs, null.combined = giantnull.obs)
+  d.obs <- data.frame(diff=wmr.obs$diff,
+                       CI.L=wmr.obs$CI.L,
+                       CI.U=wmr.obs$CI.U,
+                       timescale = wmr.obs$period,
+                       obs.length = paste(obslengths[l]))
+}
