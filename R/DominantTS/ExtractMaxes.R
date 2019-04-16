596b3ca075945ac8a736ffd4d0d102977c014b02
mcsiple <mcsiple@gmail.com>
2019-04-16 11:21:57 -0700

    dplyr-ify extractmaxes() and move to R/

--- R/DominantTS/ExtractMaxes.R
+++ R/DominantTS/ExtractMaxes.R
@@ -1,4 +1,4 @@
-library(plyr)
+#library(plyr)
 library(dplyr)
 
 #   -----------------------------------------------------------------------
@@ -18,22 +18,32 @@ extract.maxes <- function(data = alldat, region_or_subregion, scale = "Region", 
   if (scale == "Subregion") {dataset <- filter(data, subregion == region_or_subregion & datasource==data_source) }   
   
   if(variable=="landings"){
-    lt.maxes <- ddply(.data=dataset,.(sp,stock),summarize,max.var=round(max(landings,na.rm=TRUE),2))} 
+    lt.maxes <- dataset %>% 
+      group_by(sp,stock) %>% 
+      summarize(max.var=round(max(landings,na.rm=TRUE),2))}
+   
   if(variable=="ssb"){
-    lt.maxes <- ddply(.data=dataset,.(sp,stock),summarize,max.var=round(max(ssb,na.rm=TRUE),2))}
+    lt.maxes <- dataset %>% 
+      group_by(sp,stock) %>% 
+      summarize(max.var=round(max(ssb,na.rm=TRUE),2))}
+      
   if(variable=="rec"){
-    lt.maxes <- ddply(.data=dataset,.(sp,stock),summarize,max.var=round(max(rec,na.rm=TRUE),2))}
+    lt.maxes <- dataset %>% 
+      group_by(sp,stock) %>% 
+      summarize(max.var=round(max(rec,na.rm=TRUE),2))}
   if(variable=="fishing.mortality"){ # Max (F) doesn't make sense, but including it anyway to be thorough
-    lt.maxes <- ddply(.data=dataset,.(sp,stock),summarize,max.var=round(max(fishing.mortality,na.rm=TRUE),2))}
+    lt.maxes <- dataset %>% 
+      group_by(sp,stock) %>% 
+      summarize(max.var=round(max(fishing.mortality,na.rm=TRUE),2))}
   #print(lt.maxes)
   
   #anchovy stats
   lt.max.a <- max(lt.maxes[which(lt.maxes$sp=="Anchovy"),ncol(lt.maxes)],na.rm=TRUE)   
-  lt.max.sp <- lt.maxes[lt.maxes$max.var==lt.max.a,2]    # Which anchovy species had biggest long term value for this time series (i.e., the "dominant anchovy species")
+  lt.max.sp <- as.character(lt.maxes[lt.maxes$max.var==lt.max.a,2])    # Which anchovy species had biggest long term value for this time series (i.e., the "dominant anchovy species")
   
   #sardine stats
   lt.max.s <- max(lt.maxes[which(lt.maxes$sp=="Sardine"),ncol(lt.maxes)],na.rm=TRUE)
-  lt.max.sp.sar <- lt.maxes[lt.maxes$max.var==lt.max.s,2]     # "Dominant sardine species"
+  lt.max.sp.sar <- as.character(lt.maxes[lt.maxes$max.var==lt.max.s,2])     # "Dominant sardine species"
   
   dom.s.ts <- dataset[which(dataset$stock==lt.max.sp.sar),]
   dom.a.ts <- dataset[which(dataset$stock==lt.max.sp),]
@@ -51,9 +61,11 @@ extract.maxes <- function(data = alldat, region_or_subregion, scale = "Region", 
  
   
   return(list(Dom_anch_LTmax=as.character(lt.max.sp),
-              Dom_sard_LTmax=as.character(lt.max.sp.sar),dom.anch = dom.a.ts,dom.sard = dom.s.ts,max.table = lt.maxes))
+              Dom_sard_LTmax=as.character(lt.max.sp.sar),
+              dom.anch = dom.a.ts,dom.sard = dom.s.ts,
+              max.table = as.data.frame(lt.maxes)))
  
 }  #End of extract.maxes fxn
 
-# extract.maxes(data = alldat,region_or_subregion = "Benguela")
+#extract.maxes(data = alldat,region_or_subregion = "Benguela",data_source = "Barange" )
 
