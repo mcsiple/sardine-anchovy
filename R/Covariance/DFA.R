597d08a674c39c7d86c08ebf8497bef9b737f64f
mcsiple <mcsiple@gmail.com>
2019-09-19 10:20:04 -0700

    new dplyr version for getting dominant stocks; write DFA function

--- R/Covariance/DFA.R
+++ R/Covariance/DFA.R
@@ -1,15 +1,106 @@
 # Another way to get at whether the time series are asycnhronous at short time scales is to use a DFA and fit a single trend. If sardine and anchovy are asynchronous, they should have opposite loadings at the time scale you're interested in. This starts the same as the "EstimateCovariance.R" code but uses DFA instead of MARSS. 
 library(tidyverse)
-source(here::here("R/DataCleanup/getMARSSstates.R"))
+
 source(here::here("R/DataCleanup/Fill_NAs_SA.R"))
 load(here::here("R/DataCleanup/allsardineanchovy_3.RData")) # dataframe alldat
 
-region <- c("Benguela","California","Humboldt", "Kuroshio-Oyashio", "NE Atlantic")
-variable <- c("rec","ssb","landings")
-datasource <- c("Barange")
 
 
-# Set up y so that observations are lined up after one another:
+
+# First, simplify data so it's only the "dominant" sardine and anc --------
+allfishies <- alldat %>% 
+  melt(id.vars=c("datasource","scientificname","stock","year","sp","region","subregion"))
+
+domfishies <- allfishies %>%
+  group_by(region,datasource,variable,sp) %>% 
+  filter(value==max(value,na.rm=T)) %>%
+  mutate(dom_YN = 1) %>%
+  select(-value,-year) %>%
+  as.data.frame()
+
+domstocks <- left_join(allfishies,domfishies) %>% 
+  filter(dom_YN==1)
+
+domstocks %>% 
+  filter(datasource=="Barange") %>% 
+  ggplot(aes(x=year,y=value,colour=sp)) + 
+  geom_line() + 
+  facet_grid(variable~region,scales="free_y")
 
 
-# Standardize the same way as for the MARSS model
+dfa.stocks <- domstocks %>% filter(datasource=="Barange")
+
+v = "ssb" # for testing
+
+regions <- c("Benguela","California","Humboldt", "Kuroshio-Oyashio", "NE Atlantic")
+nregions=length(regions)
+maxyr <- max(dfa.stocks$year)
+minyr <- min(dfa.stocks$year)
+
+
+sa.DFA <- function(dataset = dfa.stocks,v = "ssb"){
+  #' @description takes data and returns the results of a DFA with a single trend
+  #' @param dataset (dataframe) big dataframe of sardine and anchovy time series
+  #' @param v "landings", "ssb", or "rec"
+
+    
+  # Set up data:
+  dfa.obj <- vector()
+  for(i in 1:nregions){
+    dt <- subset(dataset,variable==v & region==regions[i])
+    sard <- dt %>% filter(sp=="Sardine")
+    anch <- dt %>% filter(sp=="Anchovy")
+    sard.mars <- FillNAs.ts(ObsMat = cbind(sard$year,sard$value), # fills in missing years with NAs
+                            startyear=minyr,
+                            endyear=maxyr)
+    anch.mars <- FillNAs.ts(cbind(anch$year,anch$value),
+                            startyear=minyr,
+                            endyear=maxyr)
+    regioncombo <- rbind(sard.mars[2,],anch.mars[2,])
+    print(regions[i])
+    dfa.obj <- rbind(dfa.obj,regioncombo)
+  }
+  
+  #dfa.obj <- rbind(minyr:maxyr,dfa.obj)
+  #dfa.obj are the observations
+  dfa.obj <- zscore(log(dfa.obj))
+  
+    dfa.model=list()
+    Z.vals <- list(
+      "z1s",0,0,0,0,
+      "z1a",0,0,0,0,
+      0,"z2s",0,0,0,
+      0,"z2a",0,0,0,
+      0,0,"z3s",0,0,
+      0,0,"z3a",0,0,
+      0,0,0,"z4s",0,
+      0,0,0,"z4a",0,
+      0,0,0,0,"z5s",
+      0,0,0,0,"z5a")
+    Z = matrix(Z.vals,nrow=nregions*2,ncol=nregions,byrow=T)
+    R = "diagonal and equal" # obs error similar across species and ecosystems
+    x0 = U = A = "zero"
+    Q = "diagonal and unequal"
+    # Q.vals <- list(
+    #   "qs1","qas1",0,0,0,0,0,0,0,0,
+    #   "qas1","qa1",0,0,0,0,0,0,0,0,
+    #   0,0,"qs2","qas2",0,0,0,0,0,0,
+    #   0,0,"qas2","qa2",0,0,0,0,0,0,
+    #   0,0,0,0,"qs3","qas3",0,0,0,0,
+    #   0,0,0,0,"qas3","qa3",0,0,0,0,
+    #   0,0,0,0,0,0,"qs4","qas4",0,0,
+    #   0,0,0,0,0,0,"qas4","qa4",0,0,
+    #   0,0,0,0,0,0,0,0,"qs5","qas5",
+    #   0,0,0,0,0,0,0,0,"qas5","qa5"
+    # )
+    # Q = matrix(Q.vals,nrow=10,ncol=10,byrow=T)
+    
+    
+    dfa.model <- list(Z=Z,R=R,x0=x0,U=U,A=A,Q=Q)
+
+    fit1<-  MARSS(dfa.obj, model=dfa.model, control=list(maxit=1000))
+}
+
+sa.DFA()
+
+
