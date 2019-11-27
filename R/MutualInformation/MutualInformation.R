5e2d72c807ecb285337e962a78a87a57c7e2d531
mcsiple <mcsiple@gmail.com>
2019-11-27 12:25:33 -0800

    Initial commit

---
+++ R/MutualInformation/MutualInformation.R
@@ -0,0 +1,5 @@
+# Mutual information test
+devtools::install_github("mdscheuerell/muti")
+# Calculate mutual information for each sardine-anchvoy pair
+# Inputs: two vectors of numeric data
+# Requires: getMARSSstates function
