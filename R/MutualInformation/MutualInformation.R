f8fc1db8a9e917e5444fa40977a52eaf06cf1190
Megsie Siple <mcsiple@gmail.com>
2019-11-27 20:50:41 -0800

    Mutual information :construction:

--- R/MutualInformation/MutualInformation.R
+++ R/MutualInformation/MutualInformation.R
@@ -1,5 +1,18 @@
 # Mutual information test
 devtools::install_github("mdscheuerell/muti")
+library(muti)
+library(here)
+library(tidyverse)
 # Calculate mutual information for each sardine-anchvoy pair
 # Inputs: two vectors of numeric data
 # Requires: getMARSSstates function
+
+# Need RBF dataframe
+load(here("R","DataCleanup","RAM_Barange_FAO_States.RData")) # dataframe RBF
+
+test <- RBF %>% 
+  filter(datasource=="Barange" & 
+           region=="Benguela" & 
+           variable =="ssb")
+
+muti(x = test$Sardine.est,y = test$Anchovy.est)
